e636de53d7b8c778fe0181babde5654d2727db73
Josh <bricsio.joshua.benavides@gmail.com>
2022-03-24 18:07:59 +1000

    Created base JS operator functions. Split display into two divs.

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 let add = (x, y) => {
-
+    return x + y;
 };
 
 let subtract = (x, y) => {
-
+    return x - y;
 };
 
 let multiply = (x, y) => {
-
+    return x*y;
 };
 
 let divide = (x, y) => {
-
+    return  x/y;
 };
+
+let operate = (operator, x, y) => {
+    let result;
+    switch (operator) {
+        case "add":
+            result = x + y;
+            break;
+        case "subtract":
+            result = x - y;
+            break;
+        case "multiply":
+            result = x * y;
+            break;
+        case "divide":
+            result = x / y;
+            break;
+    }
+    return result;
+}
+
+let updateDisplay = e => {
+
+}
+
+let userInput = document.getElementById("user-input");
+userInput.addEventListener('keydown', updateDisplay);
